f3b51d21a0c2f5d9138c4050b934f5d2fbd343a0
Tom Gehrke <phyrog@gmail.com>
2019-07-22 00:05:02 +0200

    Create gateway docker file

--- src/graphql-federation-gateway/main.js
+++ src/graphql-federation-gateway/main.js
@@ -1,26 +1,8 @@
-const fs = require("fs");
-const minimist = require("minimist");
 const { ApolloGateway } = require("@apollo/gateway");
 const { ApolloServer } = require("apollo-server");
 
-const argv = minimist(process.argv.slice(2));
-
-const configFile = argv.config || "/endpoint-config/config.json";
-
-let gateway;
-let server;
-
-gateway = new ApolloGateway();
-
-/*
-storageSecretUrl = "<secret-base>/<graph_id>/storage-secret/<api-key-hash>.json"; => quoted string?
-linkFile = "<partial-base>/<secret>/<graph-variant>/v<federation-version>/composition-config-link"; => {configPath: string}
-configFileResultPath = "<partial-base>/<configPath>"; => {result: ConfigFileResult}
-serviceLocation = "<partial-base>/<parsedConfig.implementingServiceLocations.<key>>"; => {url: string, partialSchemaPath: string}
-schemaPath = <partial-base>/<partialSchemaPath>; => graphql schema
-*/
-
-server = new ApolloServer({
+const gateway = new ApolloGateway();
+const server = new ApolloServer({
   gateway: gateway,
   subscriptions: false,
   engine: {
@@ -31,6 +13,6 @@ server = new ApolloServer({
     endpointUrl: "http://localhost:8000"
   }
 });
-server.listen().then(url => {
+server.listen().then(({url}) => {
   console.log(`Listening to ${url}.`);
 });
